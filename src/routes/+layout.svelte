ccbd6ef76a82f42c7160f058cc96d91e27d09719
Austin <141854239+DiceBreakers@users.noreply.github.com>
2025-06-19 16:10:51 -0500

    Analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,16 +3,11 @@
   // Initialize analytics on the client
   injectAnalytics();
 
-  export let data: { user: any; path: string };
 </script>
 
 <header>
   <h1>Stray To Play Rescue</h1>
-  {#if data.user}
-    <nav>Welcome, {data.user.username}</nav>
-  {:else}
-    <nav><a href="/login">Log in</a></nav>
-  {/if}
+Save the dogs!!!
 </header>
 
 <slot />
